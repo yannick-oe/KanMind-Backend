ea550fc8aea73b151e02f63a66ac62cf9f0267fc
Yannick Oe <yannickoetelshoven@gmail.com>
2026-07-24 08:21:53 +0200

    chore(core): register kanban_app and route board urls

--- core/settings.py
+++ core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "auth_app",
+    "kanban_app",
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("auth_app.api.urls")),
+    path("api/", include("kanban_app.api.urls")),
 ]
